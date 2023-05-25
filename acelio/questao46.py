de53aa106f2004e6f263c926d055ccd7c9ce6801
yohane <yohanecavalcantex@gmail.com>
2023-05-25 18:04:25 -0300

    questao 46

---
+++ acelio/questao46.py
@@ -0,0 +1,10 @@
+dict = {
+    "Alto nível" : "Python",
+    "Médio nível": "C",
+    "Baixo nível": "Assembly"
+}
+print(dict)
+if "Python" in dict.values() :
+    print('Python está na lista.\n')
+else :
+    print("Python não está na lista.\n")
