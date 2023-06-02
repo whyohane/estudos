42c172e10fc27f9d7703c92c39b48f507539eea6
yohane <yohanecavalcantex@gmail.com>
2023-06-02 13:09:33 -0300

    criar lista 5 números inteiros

---
+++ python.org/Lista de Listas/questao1.py
@@ -0,0 +1,9 @@
+'''Faça um Programa que leia um vetor de 5 números inteiros e mostre-os. '''
+
+lista = []
+
+for i in range(5) :
+    num = int(input("Digite um número inteiro.\n"))
+    lista.append(num)
+
+print("A lista de 5 números é:{}".format(lista))
