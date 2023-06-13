3ab41aeae9501ff7116ee5b4708691360d0649a0
yohane <yohanecavalcantex@gmail.com>
2023-06-13 15:38:59 -0300

    -

---
+++ w3resource/list exercises/exercise1.py
@@ -0,0 +1,11 @@
+# Exercise 
+
+'''1 - rewrite the above example code using list comprehension syntax. Make a variable named 
+uppercased_fruits to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]'''
+
+fruits = ['mango', 'kiwi', 'strawberry', 'guava', 'pineapple', 'mandarin orange']
+
+uppercased_fruits = [x.upper() for x in fruits]
+
+print(fruits)
+print(uppercased_fruits)
