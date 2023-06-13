7416042363554948c959a7c748715478cee37447
yohane <yohanecavalcantex@gmail.com>
2023-06-13 15:40:41 -0300

    -

--- w3resource/list exercises/exercise1.py
+++ w3resource/list exercises/exercise1.py
@@ -1,4 +1,4 @@
-# Exercise 
+# Exercise 1
 
 '''1 - rewrite the above example code using list comprehension syntax. Make a variable named 
 uppercased_fruits to hold the output of the list comprehension. Output should be ['MANGO', 'KIWI', etc...]'''
