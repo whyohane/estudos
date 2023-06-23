8b81f754eb28a31934b413569ff28c5ebdeef5f1
yohane <yohanecavalcantex@gmail.com>
2023-06-23 11:40:48 -0300

    -

---
+++ acelio/desafio4.py
@@ -0,0 +1,10 @@
+'''4.Faça um programa que leia duas listas e que gere uma terceira com os elementos das duas primeiras.'''
+
+lista1 = [1, 2, 3, 4, 5]
+lista2 = [6, 7, 8, 9, 10]
+lista3 = lista1 + lista2
+
+print(lista1)
+print(lista2)
+
+print("A soma das duas listas é: {}".format(lista3))
