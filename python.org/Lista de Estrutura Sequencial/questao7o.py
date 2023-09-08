b687da8084464a54eec3c12559bb9b4251b6616d
Yohane Cavalcante <119906675+whyohane@users.noreply.github.com>
2023-09-08 11:32:19 -0300

    área de um quadrado e dobro da área

--- python.org/Lista de Estrutura Sequencial/questao7o.py
+++ python.org/Lista de Estrutura Sequencial/questao7o.py
@@ -1,4 +1,5 @@
 # Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário. 
+
 print("Programa para calcular a área de um quadrado. \n")
 lado = float(input("Digite o valor do lado do quadrado: \n"))
 area = lado * lado
