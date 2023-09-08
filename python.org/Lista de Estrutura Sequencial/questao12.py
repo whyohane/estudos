c816e6d2ddbca1f3bd19ffb1c634ad20a5279855
Yohane Cavalcante <119906675+whyohane@users.noreply.github.com>
2023-09-08 11:37:19 -0300

    cálculo de peso ideal

--- python.org/Lista de Estrutura Sequencial/questao12.py
+++ python.org/Lista de Estrutura Sequencial/questao12.py
@@ -1,4 +1,5 @@
 # Tendo como dados de entrada a altura de uma pessoa, construa um algoritmo que calcule seu peso ideal, usando a seguinte fórmula: (72.7*altura) - 58 
+
 print("Programa para calcular o peso ideal de uma pessoa. \n")
 altura = float(input("Digite a sua altura: \n"))
 peso = ((72.7*altura) - 58)
