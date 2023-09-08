51978e705cbe91b081dfb7edde488199cc5ab6fa
Yohane Cavalcante <119906675+whyohane@users.noreply.github.com>
2023-09-08 11:34:25 -0300

    transformação de graus fahrenheit para celsius

--- python.org/Lista de Estrutura Sequencial/questao9.py
+++ python.org/Lista de Estrutura Sequencial/questao9.py
@@ -1,4 +1,5 @@
 # Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius. 
+
 print("Programa para transformar graus Fahrenheit em graus Celsius : \n")
 fahrenheit = float(input("Digite o valor em graus fahrenheit: \n"))
 celsius = 5 * ((fahrenheit-32) / 9)
