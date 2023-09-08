6eaba3a14b22a0fc5f7bf61807ddaad6b7a8e78a
Yohane Cavalcante <119906675+whyohane@users.noreply.github.com>
2023-09-08 11:25:54 -0300

    média de 4 notas

--- python.org/Lista de Estrutura Sequencial/questao4.py
+++ python.org/Lista de Estrutura Sequencial/questao4.py
@@ -1,4 +1,5 @@
 # Faça um Programa que peça as 4 notas bimestrais e mostre a média. 
+
 print("Programa para calcular média: \n")
 n1 = float(input("Digite a primeira nota: \n"))
 n2 = float(input("Digite a segunda nota: \n"))
