c0ef6c3ff89af7e6eec2dc813f93e28d4dab5855
Yohane Cavalcante <119906675+whyohane@users.noreply.github.com>
2023-09-08 11:24:33 -0300

    soma de dois números

--- python.org/Lista de Estrutura Sequencial/questao3.py
+++ python.org/Lista de Estrutura Sequencial/questao3.py
@@ -1,4 +1,5 @@
 # Faça um Programa que peça dois números e imprima a soma. 
+
 print("Programa para somar dois números.\n")
 x = int(input("Digite o primeiro número: \n"))
 y = int(input("Digite o segundo número: \n"))
